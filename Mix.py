8fb4a2e932cf95abe76dc8d0ffda91e85777b5cf
HADI ANHAF AIMAN <117533758+AKASH-110@users.noreply.github.com>
2022-11-06 11:03:14 +0600

    Create Mix.py

---
+++ Mix.py
@@ -0,0 +1,43 @@
+import sys,time
+
+from platform import uname
+
+from os import path,system
+
+arch=uname().machine.lower()
+
+if "aarch" in arch:
+
+    arch="aarch"
+
+    print('\033[1;32m\nCongratulatings! Your Deviec Support This Tools');time.sleep(1)
+
+else:
+
+    print('\033[1;31m\nSorry System not support this tools');sys.exit()
+
+print('\n\n\033[1;37m[•] This tools only for 64bit device ')
+
+print('\n[1] Start Cloning V1.3.9 \n[2] Start Random Cloning V1.3.8 (Updated)\n[3] Check Update \n')
+
+xd=input('[•] choose: ')
+
+if xd in ['1','01']:
+
+    if path.isfile('free.cpython-311.so'):
+
+        import free
+
+    
+
+elif xd in ['2','02']:
+
+    if path.isfile('mb1.cpython-311.so'):
+
+        import mb1    
+
+       
+
+        print('\n[•] Checking updates...')
+
+        system('python Mix.py update')
